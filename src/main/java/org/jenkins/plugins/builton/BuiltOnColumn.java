77e4b59d61edefcf17a85fc00be627e6360dc475
Henk van Voorthuijsen <voorth@xs4all.nl>
2011-05-01 14:52:53 +0200

    First commit

---
+++ src/main/java/org/jenkins/plugins/builton/BuiltOnColumn.java
@@ -0,0 +1,20 @@
+package org.jenkins.plugins.builton;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.views.ListViewColumn;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class BuiltOnColumn extends ListViewColumn{
+	@DataBoundConstructor
+	public BuiltOnColumn() {}
+	
+    @Extension
+    public static class DescriptorImpl extends Descriptor<ListViewColumn> {
+        @Override
+        public String getDisplayName() {
+            return "Built On";
+        }
+    }
+}
